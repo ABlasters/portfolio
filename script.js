fb8b6a4b6dad352864decf7a921110f7a5f8f1cc
ablasters <100992149+ABlasters@users.noreply.github.com>
2024-12-22 12:31:11 +0100

    variable diff devient age

--- script.js
+++ script.js
@@ -5,13 +5,6 @@ const dateActuelle = new Date();
 const dateNaissance = new Date('2009-06-27');
 
 // Calculer la différence entre les deux dates
-const diff = new Date(dateActuelle - dateNaissance);
-
-// Extraire les années, mois et jours de la différence
-const age = {
-    years: diff.getUTCFullYear() - 1970,
-    months: diff.getUTCMonth(),
-    days: diff.getUTCDate() - 1
-};
+const age = new Date(dateActuelle - dateNaissance);
 
 console.log(`Âge: ${age.years} ans, ${age.months} mois, ${age.days} jours`);
